b8eb002ab5e3fa5676543dbfdd03d76774c189b6
KOMPUTER\Yurik <yuriydanyuk@gmail.com>
2025-03-31 10:35:34 +0300

    #Added ValidParentheses2

--- Lesson2/ValidParentheses.go
+++ Lesson2/ValidParentheses.go
@@ -7,12 +7,12 @@ import (
 func main() {
 	fmt.Println(ValidParentheses("()"))     // true
 	fmt.Println(ValidParentheses("()[]{}")) // true
-	fmt.Println(ValidParentheses("(]"))     // false
-	fmt.Println(ValidParentheses("([)]"))   // false
-	fmt.Println(ValidParentheses("{[]}"))   // true
-	fmt.Println(ValidParentheses("[{}]"))   // true
-	fmt.Println(ValidParentheses("[{]}"))   // false
-	fmt.Println(ValidParentheses("((((((((((((((((((((((((((((((((((((((((((((((((("))
+	fmt.Println(ValidParentheses2("(]"))    // false
+	fmt.Println(ValidParentheses2("([)]"))  // false
+	fmt.Println(ValidParentheses2("{[]}"))  // true
+	fmt.Println(ValidParentheses2("[{}]"))  // true
+	fmt.Println(ValidParentheses2("[{]}"))  // false
+	fmt.Println(ValidParentheses2("((((((((((((((((((((((((((((((((((((((((((((((((("))
 }
 
 func ValidParentheses(s string) bool {
@@ -33,5 +33,32 @@ func ValidParentheses(s string) bool {
 			stack = stack[:len(stack)-1]
 		}
 	}
+	return len(stack) == 0
+}
+
+func ValidParentheses2(s string) bool {
+	stack := []rune{}
+	brackets := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	for _, char := range s {
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')', ']', '}':
+			open, ok := brackets[char]
+			if !ok {
+				return false // Invalid closing bracket
+			}
+			if len(stack) == 0 || stack[len(stack)-1] != open {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
 	return len(stack) == 0
 }
